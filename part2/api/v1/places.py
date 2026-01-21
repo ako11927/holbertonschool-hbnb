eeadb3103580040ae8acb8a40ad3e20c3772286a
Rinadfahadz <rinad-alzuaybir@outlook.com>
2026-01-21 13:12:06 +0300

    upd

--- part2/api/v1/places.py
+++ part2/api/v1/places.py
@@ -9,7 +9,7 @@ amenity_model = api.model('PlaceAmenity', {
     'id': fields.String(description='Amenity ID'),
     'name': fields.String(description='Name of the amenity')
 })
-
+  
 user_model = api.model('PlaceUser', {
     'id': fields.String(description='User ID'),
     'first_name': fields.String(description='First name of the owner'),
