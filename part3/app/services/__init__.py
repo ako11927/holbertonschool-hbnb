8f4d6da6675e87590b8b7346db79b50eefe79ca0
Shadan Khaled <shadankhaled75@gmail.com>
2026-02-16 09:50:19 +0300

    Add Part2, Part3, and Part4 for final project

--- part3/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .facade import HBnBFacade
-
-# Create a singleton instance
-facade = HBnBFacade()
--- part3/part2/backup/app_backup/api/v1/__init__.py
+++
@@ -1,4 +0,0 @@
-"""API v1 package initialization."""
-from .routes import api
-
-__all__ = ['api']
